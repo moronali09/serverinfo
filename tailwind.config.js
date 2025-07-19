17ed6d3814a84db2f27593371f4900e1129401e9
moronali09 <jonibou04@gmail.com>
2025-07-20 01:15:25 +0600

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  darkMode: 'class',
-  content: ["./src/pages/**/*.{js,jsx}", "./src/components/**/*.{js,jsx}", "./src/styles/**/*.css"],
-  theme: { extend: {} },
-  plugins: [],
-};
